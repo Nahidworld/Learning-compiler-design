2b1cf1fcc806ac661e5f293e9e411dce530e9772
Nahidworld <157556719+Nahidworld@users.noreply.github.com>
2024-01-31 10:25:45 +0600

    Add files via upload

---
+++ Lab 1/lab1.cpp
@@ -0,0 +1,14 @@
+///Find the average value do the elements of an array.*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double arr[5];
+    for(int i=0;i<=5;i++)
+    {
+        cin>>arr[i];
+    }
+
+    return 0;
+}
